77082c1000b66d3968db26cbcceba1e0b87b5a0e
bryce <brycekbargar@gmail.com>
2016-04-30 21:02:24 -0500

    Implement the Fibonacci module

---
+++ 02_chapter/Fibonacci.elm
@@ -0,0 +1,20 @@
+module Fibonacci where
+
+
+import List exposing ((::), reverse, map2)
+
+
+fibonacci : Int -> List Int
+fibonacci n = 
+    let fibonacci' n k1 k2 acc =
+        case n of
+            0 -> acc
+            _ -> fibonacci' (n-1) k2 (k1+k2) (k2::acc)
+    in 
+        fibonacci' n 0 1 [] |> reverse 
+
+
+fibonacciWithIndexes : Int -> List (Int, Int)
+fibonacciWithIndexes n = 
+    fibonacci n |> map2 (,) [0..n]
+    
