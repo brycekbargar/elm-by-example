b1fb2fbce8d4eeb603ce19e701c533d2f072137b
bryce <brycekbargar@gmail.com>
2016-05-04 20:03:44 -0500

    Implement EyesView

---
+++ 05_chapter/EyesView.elm
@@ -0,0 +1,49 @@
+module EyesView where
+
+
+import Color exposing (Color, black, white)
+import Graphics.Collage exposing (Form, collage, oval, filled, group, move, moveX)
+import Graphics.Element exposing (Element)
+
+
+type alias Size = (Int, Int)
+type alias Location = (Float, Float)
+
+
+coloredOval : Color -> Float -> Float -> Form
+coloredOval c w h = oval w h |> filled c
+
+
+border : Float -> Float -> Form
+border w h = 
+    let 
+        outer = coloredOval black w h 
+        inner = coloredOval white (w * 9/10) (h * 9/10) 
+    in 
+        group [ outer, inner ] 
+
+
+pupil : Float -> Float -> Form
+pupil w h = coloredOval black (w/5) (h/5)
+
+
+eyesView : Size -> Location -> Location -> Element
+eyesView (w, h) l r = 
+    let 
+        eyeWidth = (toFloat w) / 2
+        eyeOffset = eyeWidth / 2
+        eyeHeight = toFloat h
+    in
+        [
+            border eyeWidth eyeHeight |> moveX -eyeOffset
+            ,pupil eyeWidth eyeHeight |> move l
+            ,border eyeWidth eyeHeight |> moveX eyeOffset
+            ,pupil eyeWidth eyeHeight |> move r
+        ]
+            |> collage w h
+
+
+{--
+main : Element
+main = eyesView (700, 500) (-50, -50) (100, 100)
+--}
